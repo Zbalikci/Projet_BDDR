437365e8565c9665594a313c7c1f37b30937cbb0
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2023-03-13 13:34:28 +0100

    Update Peuplement_essais.py

--- Peuplement_essais.py
+++ Peuplement_essais.py
@@ -23,4 +23,15 @@ for dossier in dossiers[1:-1]:
     for element in elements :
         print(element[:-4])
     
+ 
+#!/bin/env python3
+import pandas as pd
+import logging
+import psycopg2
+
+df=pd.read_csv("/users/2023/ds1/share/CORD-19/metadata.csv")
+
+for i in range(100):
+	print(f'title = {df["title"][i]}, abstract = {df["abstract"][i]}, url = {df["url"][i]} , publication_date= {df["publish_time"][i]}, journal = {df["journal"][i]}, authors = {df["authors"][i]}\n')
+
 
