dcbb0e8d8d4906875f1f611ce5c476489842c740
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2023-03-17 20:49:24 +0100

    Update models.py

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Sous_Theme(models.Model):
 		return self.name
 
 class Journal(models.Model):
-	name=models.CharField(max_length=300, unique=True)
+	name=models.CharField(max_length=500, unique=True)
 	def __str__(self):
 		return self.name
 	def __repr__(self):
