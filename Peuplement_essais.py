752b7c886a311b64bad245e9ba5d67b0519ef91a
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2023-03-17 20:30:54 +0100

    Update Peuplement_essais.py

--- Peuplement_essais.py
+++ Peuplement_essais.py
@@ -7,59 +7,26 @@ import json
 from django.db import models
 
 """
-Pour peupler les tables theme, sous_theme et journal:
+Pour peupler les tables theme, sous_theme, studytype, affiliation et journal:
 """
+print("Où se trouve vos fichiers/dossiers Kaggle, documents_parses et metadata.csv ?")
+print("Par exemple : 'D:/archive/Kaggle'   ou  'D:/archive/metadata.csv'")
+print("Dans ce cas : chemin_archive = 'D:/archive' ")
+chemin_archive = input("Veuillez donner le chemin : chemin_archive= ") or "D:/archive"
 
-chemin = "/users/2023/ds1/share/CORD-19/Kaggle/target_tables"
-elements = os.listdir(chemin)
-dossiers = [element for element in elements if os.path.isdir(os.path.join(chemin, element))]
-df=pd.read_csv("/users/2023/ds1/share/CORD-19/metadata.csv")
+chemin_tables=f'{chemin_archive}/Kaggle/target_tables'
+elements = os.listdir(chemin_tables)
+dossiers = [element for element in elements if os.path.isdir(os.path.join(chemin_tables, element))]
+print("En train de charger le fichier metadata.csv")
+df=pd.read_csv(f'{chemin_archive}/metadata.csv')
+print("En train de récupérer la liste des journaux dans metadata.csv")
 l=df["journal"][:100].unique()
 
-try:
-	connection = psycopg2.connect("host=data dbname=zbalikci user=zbalikci password=zbalikci")
-	cursor = connection.cursor()
-	for dossier in dossiers[1:-1]:
-		theme=(dossier[2:].replace("_"," ")).upper()
-		cursor.execute("""
-		INSERT INTO appli_covid19_theme(name)
-		VALUES(%s)
-		RETURNING id;
-		""",
-		(theme,)) # IMPORTANT LORSQU'IL Y A UN SEUL VALEUR !!! RAJOUTER VIRGULE , !!!!
-		id_theme=cursor.fetchone()[0]
-		chemin = f'/users/2023/ds1/share/CORD-19/Kaggle/target_tables/{dossier}'
-		elements = os.listdir(chemin)
-		for element in elements :
-			cursor.execute("""
-			INSERT INTO  appli_covid19_sous_theme(name,theme_id)
-			VALUES(%s,%s);
-			""",
-			(f'{element[:-4]}',id_theme))
-			
-	for i in range(len(l)):
-		cursor.execute("""
-		INSERT INTO appli_covid19_journal(name)
-		VALUES(%s);
-		""",
-		(l[i],))
-	connection.commit()
-	connection.close()
+print("En train de crée la liste pour peupler la table studytype")
 
-except Exception as e:
-    logging.error("database connection failed")
-    logging.error(e)
-
-"""
-Pour peupler la tables studytype:
-"""
-
-chemin = "/users/2023/ds1/share/CORD-19/Kaggle/target_tables"
-elements = os.listdir(chemin)
-dossiers = [element for element in elements if os.path.isdir(os.path.join(chemin, element))]
 liste=[]
 for dossier in dossiers[1:-1]:
-	chemin = f'/users/2023/ds1/share/CORD-19/Kaggle/target_tables/{dossier}'
+	chemin = f'{chemin_tables}/{dossier}'
 	elements = os.listdir(chemin)
 	for element in elements :
 		df=pd.read_csv(f'{chemin}/{element}')
@@ -69,68 +36,150 @@ for dossier in dossiers[1:-1]:
 				liste.append(t)
 		except:
 			pass
+liste=list(set(liste))  
 
-liste=list(set(liste))
-
-
-try:
-	connection = psycopg2.connect("host=data dbname=zbalikci user=zbalikci password=zbalikci")
-	cursor = connection.cursor()
-	for study in liste:
-		cursor.execute("""
-		INSERT INTO appli_covid19_studytype(name)
-		VALUES(%s);
-		""",
-		(study,)) # IMPORTANT LORSQU'IL Y A UN SEUL VALEUR !!! RAJOUTER VIRGULE , !!!!
-	connection.commit()
-	connection.close()
-
-except Exception as e:
-    logging.error("database connection failed")
-    logging.error(e)
-    
-
-chemin1 = "/users/2023/ds1/share/CORD-19/document_parses/pdf_json"
-elements = os.listdir(chemin1)
-#essayer aussi avec les fichier dans le dossier : #les auteurs dans le pmc_json semble ne pas avoir d'affiliation
-chemin2 = "/users/2023/ds1/share/CORD-19/document_parses/pmc_json"
+print("\nESSAI PEUPLEMENT DEBUT\n")
 
 try:
-	connection = psycopg2.connect("host=data dbname=zbalikci user=zbalikci password=zbalikci")
-	cursor = connection.cursor()
-	cursor.execute("""
-	INSERT INTO appli_covid19_affiliation(name,type,location)
-	VALUES(%s,%s,%s);
-	""",
-	('NULL','NULL','NULL'))
-	for element in elements[:50]:
-		with open(f'{chemin1}/{element}', 'r') as f:
-			data = json.load(f)
-		if len(data['metadata']['authors'])!=0:
-			for i in range(len(data['metadata']['authors'])):
-				l= data['metadata']['authors'][i]['affiliation']
-				if l!={}:
-					if l['institution'] != '':
-						name = l['institution']
-						type='institution'
-					else:
-						name = l['laboratory']
-						type='laboratory'
-					L = l['location']
-					c=L.values()
-					k=[j for j in c]
-					location=" ".join(str(K).upper() for K in k)
-					cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name,))
-					records = cursor.fetchall()
-					if records==[]:
-						cursor.execute("""
-						INSERT INTO appli_covid19_affiliation(name,type,location)
-						VALUES(%s,%s,%s);
-						""",
-						(name,type.upper(),location.upper())) 
-	connection.commit()
-	connection.close()
+    connection = psycopg2.connect("host=localhost dbname=covid19 user=postgres password=Zor.bulursun1")
+    cursor = connection.cursor()
+    print("Création d'individus NULL pour les tables theme, sous_theme, studytype et affiliation :")
+    cursor.execute("""SELECT * FROM appli_covid19_theme WHERE name LIKE %s""", ('NULL',))
+    records = cursor.fetchall()
+    if records==[]:
+        cursor.execute("""INSERT INTO appli_covid19_theme(name) VALUES(%s) RETURNING id;""",('NULL',)) 
+        id_theme=cursor.fetchone()[0]
+    else :
+        id_theme=records[0][1]
+    cursor.execute("""SELECT * FROM appli_covid19_sous_theme WHERE name LIKE %s""", ('NULL',))
+    records = cursor.fetchall()
+    if records==[]:
+        cursor.execute("""INSERT INTO appli_covid19_sous_theme(name,theme_id) VALUES(%s,%s);""",('NULL',id_theme))
+
+    cursor.execute("""SELECT * FROM appli_covid19_studytype WHERE name LIKE %s""", ('NULL',))
+    records = cursor.fetchall()
+    if records==[]:
+        cursor.execute("""INSERT INTO appli_covid19_studytype(name) VALUES(%s);""",('NULL',))
 	
+    cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", ('NULL',))
+    records = cursor.fetchall()
+    if records==[]:
+        cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",('NULL','NULL','NULL'))
+    print("Individu NULL pour les tables theme, sous_theme, studytype et affiliation a été crée")
+    print('peuplement des tables themes et sous_themes : début')
+    for dossier in dossiers[1:-1]:
+        theme=(dossier[2:].replace("_"," ")).upper()
+
+        cursor.execute("""SELECT * FROM appli_covid19_theme WHERE name LIKE %s""", (theme,))
+        records = cursor.fetchall()
+        if records==[]:
+            cursor.execute("""INSERT INTO appli_covid19_theme(name) VALUES(%s) RETURNING id;""",(theme,)) 
+            id_theme=cursor.fetchone()[0]
+        else:
+            id_theme=records[0][1]
+        chemin = f'{chemin_tables}/{dossier}'
+        elements = os.listdir(chemin)
+        for element in elements :
+            cursor.execute("""SELECT * FROM appli_covid19_sous_theme WHERE name LIKE %s""", (f'{element[:-4]}',))
+            records = cursor.fetchall()
+            if records==[]:
+                cursor.execute("""INSERT INTO  appli_covid19_sous_theme(name,theme_id) VALUES(%s,%s);""",(f'{element[:-4]}',id_theme))
+    print('peuplement des tables themes et sous_themes : fin')
+    print('peuplement de la table studytype : début')
+    for study in liste:
+        cursor.execute("""SELECT * FROM appli_covid19_studytype WHERE name LIKE %s""", (str(study),))
+        records = cursor.fetchall()
+        if records==[]:
+            cursor.execute("""INSERT INTO appli_covid19_studytype(name) VALUES(%s);""",(str(study),))
+    print('peuplement de la table studytype : fin')
+    print('peuplement de la table journal : début')
+    for i in range(len(l)):
+        cursor.execute("""SELECT * FROM appli_covid19_journal WHERE name LIKE %s""", (l[i],))
+        records = cursor.fetchall()
+        if records==[]:
+            cursor.execute("""INSERT INTO appli_covid19_journal(name) VALUES(%s);""",(l[i],))
+    print('peuplement de la table journal : fin')
+
+    chemin1 = f'{chemin_archive}/document_parses/pdf_json'
+    elements1 = os.listdir(chemin1)
+    #les auteurs dans le pmc_json semble ne pas avoir d'affiliation
+    chemin2 = f'{chemin_archive}/document_parses/pmc_json'
+    elements2 = os.listdir(chemin2)
+    print('peuplement de la table affiliation : début')
+    for element in elements1[:50]:
+        with open(f'{chemin1}/{element}', 'r') as f:
+            data = json.load(f)
+        if len(data['metadata']['authors'])!=0:
+            for i in range(len(data['metadata']['authors'])):
+                l= data['metadata']['authors'][i]['affiliation']
+                if l!={}:
+                    if l['institution'] == '':
+                        name = l['laboratory']
+                        typ='laboratory'
+                    elif l['laboratory']== '':
+                        name = l['institution']
+                        typ='institution'
+                    else:
+                        name=[]
+                        name.append(l['laboratory'])
+                        name.append(l['institution'])
+                    L = l['location']
+                    c=L.values()
+                    k=[j for j in c]
+                    location=" ".join(str(K).upper() for K in k)
+                    if type(name) == list: 
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name[0],))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name[0],'laboratory'.upper(),location.upper()))
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name[1],))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name[1],'institution'.upper(),location.upper()))
+                    else:
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name,))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name,typ.upper(),location.upper()))
+    for element in elements2[:50]:
+        with open(f'{chemin2}/{element}', 'r') as f:
+            data = json.load(f)
+        if len(data['metadata']['authors'])!=0:
+            for i in range(len(data['metadata']['authors'])):
+                l= data['metadata']['authors'][i]['affiliation']
+                if l!={}:
+                    if l['institution'] == '':
+                        name = l['laboratory']
+                        typ='laboratory'
+                    elif l['laboratory']== '':
+                        name = l['institution']
+                        typ='institution'
+                    else:
+                        name=[]
+                        name.append(l['laboratory'])
+                        name.append(l['institution'])
+                    L = l['location']
+                    c=L.values()
+                    k=[j for j in c]
+                    location=" ".join(str(K).upper() for K in k)
+                    if type(name) == list: 
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name[0],))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name[0],'laboratory'.upper(),location.upper()))
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name[1],))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name[1],'institution'.upper(),location.upper()))
+                    else:
+                        cursor.execute("""SELECT * FROM appli_covid19_affiliation WHERE name LIKE %s""", (name,))
+                        records = cursor.fetchall()
+                        if records==[]:
+                            cursor.execute("""INSERT INTO appli_covid19_affiliation(name,type,location) VALUES(%s,%s,%s);""",(name,typ.upper(),location.upper()))
+    print('peuplement de la table affiliation : fin')
+    connection.commit()
+    connection.close()
+
 except Exception as e:
     logging.error("database connection failed")
     logging.error(e)
