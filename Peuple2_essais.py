b028be5fae919d7375b1175231574b4a9585ac02
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2023-03-18 15:16:22 +0100

    Create Peuple2_essais.py

---
+++ Peuple2_essais.py
@@ -0,0 +1,7 @@
+"""
+Pour peupler les tables
+"""
+df=pd.read_csv("/users/2023/ds1/share/CORD-19/metadata.csv")
+
+for i in range(100):
+	print(f'title = {df["title"][i]}, abstract = {df["abstract"][i]}, url = {df["url"][i]} , publication_date= {df["publish_time"][i]}, journal = {df["journal"][i]}, authors = {df["authors"][i]}\n')
