92320864c849f5bd9f146187d64f361167d71ec2
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2023-04-07 14:53:42 +0200

    Update models.py

--- models.py
+++ models.py
@@ -42,8 +42,7 @@ class Affiliation(models.Model):
 
 class Authors(models.Model):
 	name=models.CharField(max_length=100, unique=True)
-	email=models.CharField(max_length=100, unique=True,null=True)
-	affliation=models.ForeignKey(Affiliation, on_delete=models.CASCADE)
+	email=models.CharField(max_length=100,null=True)
 	def __str__(self):
 		return self.name
 	def __repr__(self):
@@ -54,7 +53,6 @@ class Articles(models.Model):
 	publication_date=models.DateTimeField(null=True)
 	abstract=models.TextField()
 	stulink=models.URLField(null=True)
-	studytype=models.ForeignKey(StudyType, on_delete=models.CASCADE)
 	journal=models.ForeignKey(Journal, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.title
@@ -68,8 +66,12 @@ class Author_Article(models.Model):
 class Article_Theme(models.Model):
 	sous_theme=models.ForeignKey(Sous_Theme, on_delete=models.CASCADE)
 	article=models.ForeignKey(Articles, on_delete=models.CASCADE)
-	
-	
+
 class Author_Affiliation(models.Model):
 	author=models.ForeignKey(Authors, on_delete=models.CASCADE)
-	affliation=models.ForeignKey(Affiliation, on_delete=models.CASCADE)
+	affiliation=models.ForeignKey(Affiliation, on_delete=models.CASCADE)
+	
+class StudyType_Articles(models.Model):
+	studytype=models.ForeignKey(StudyType, on_delete=models.CASCADE)
+	article=models.ForeignKey(Articles, on_delete=models.CASCADE)
+
