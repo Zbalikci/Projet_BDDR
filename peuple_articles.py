aab593148057cdb836694b30f113dcac291e9ce1
mariama24 <114244141+mariama24@users.noreply.github.com>
2023-04-12 12:34:31 +0200

    Update peuple_articles.py

--- peuple_articles.py
+++ peuple_articles.py
@@ -6,13 +6,13 @@ Created on Fri Apr  7 15:05:30 2023
 @author: mmbay
 """
 
-from appli_covid19.models import Articles,Journal 
+from appli_covi.models import Articles,Journal,StudyType_Articles,StudyType
 from django.db import IntegrityError
 from datetime import datetime
 import pandas as pd
 
 data =pd.read_csv("/Users/mariamadiabymbaye/Downloads/metadata2.csv",
-                  usecols=['title','publish_time','abstract','url','journal'])
+                  usecols=['title','publish_time','abstract','url','journal','studytype'])
 
 # Remplissage table Arcticles
 for i in range(len(data['title'])):
@@ -38,6 +38,19 @@ for i in range(len(data['title'])):
     un_article.stulink = data['url'][i]
     un_article.journal = un_journal
     un_article.save()
-
+    
+    ############ StudyType ################
+    un_studytype = StudyType()
+    try:
+        un_studytype.name = data['studytype'][i]
+        un_studytype.save() 
+    except IntegrityError:
+        un_studytype = StudyType.objects.get(name = data['studytype'][i])
+    
+    ####### StudyType_Articles ########
+    un_studyarticle = StudyType_Articles()
+    un_studyarticle.article = un_article
+    un_studyarticle.studytype = un_studytype
+    un_studyarticle.save()
     
     
